c5e11bc765cb38d07f34dc8e4a08780ab59efde1
Herman Gutierrez <hermangutierrez@gmail.com>
2024-11-30 11:43:11 -0500

    Bienes Raices HTML, CSS and JavaScript

---
+++ README.md
@@ -0,0 +1,7 @@
+Pagina web de bienes raices desarrollado con HTML, CSS y JavaScript. 
+
+Con paginas de Inicio, de nosotros, anuncios donde aparecen las propiedades, pagina de descripcion de propiedad, pagina de blog, y pagina de contacto donde se creò un formulario para que el usuario puda contactarnos.
+
+Tiene modo de Dark Mode.
+
+Resposive adaptable a todo tipo de pantallas.
---
+++ build/js/js.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){
+    eventListeners();
+    darkMode();
+});
+
+function darkMode() {
+    const botonDarkMode = document.querySelector(".dark-mode-boton");
+
+    botonDarkMode.addEventListener("click", function(){
+        document.body.classList.toggle("dark-mode");
+    });
+}
+
+function eventListeners() {
+    const mobilMenu=document.querySelector(".mobil-menu");
+
+    mobilMenu.addEventListener("click", navegacionResponsive);
+}
+
+function navegacionResponsive(){
+    const navegacion=document.querySelector(".navegacion");
+
+    navegacion.classList.toggle("mostrar");
+}
